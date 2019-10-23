137b72e6113bb7deca853fd652367b1f59966570
Viktor Kuzhelny <viktor@marketto.ru>
2019-10-23 14:36:37 +0500

    v1.0.0

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <nav-bar/>
+    <aside-menu :menu="menu"/>
+    <router-view/>
+    <footer-bar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar'
+import AsideMenu from '@/components/AsideMenu'
+import FooterBar from '@/components/FooterBar'
+
+export default {
+  name: 'home',
+  components: {
+    FooterBar,
+    AsideMenu,
+    NavBar
+  },
+  computed: {
+    menu () {
+      return [
+        'General',
+        [
+          {
+            to: '/',
+            icon: 'desktop-mac',
+            label: 'Dashboard'
+          }
+        ],
+        'Examples',
+        [
+          {
+            to: '/tables',
+            label: 'Tables',
+            icon: 'table',
+            updateMark: true
+          },
+          {
+            to: '/forms',
+            label: 'Forms',
+            icon: 'square-edit-outline'
+          },
+          {
+            to: '/profile',
+            label: 'Profile',
+            icon: 'account-circle'
+          },
+          {
+            label: 'Submenus',
+            subLabel: 'Submenus Example',
+            icon: 'view-list',
+            menu: [
+              {
+                href: '#void',
+                label: 'Sub-item One'
+              },
+              {
+                href: '#void',
+                label: 'Sub-item Two'
+              }
+            ]
+          }
+        ],
+        'About',
+        [
+          {
+            href: 'https://admin-null.justboil.me',
+            label: 'Premium Demo',
+            icon: 'credit-card'
+          },
+          {
+            href: 'https://justboil.me/bulma-admin-template/null',
+            label: 'About',
+            icon: 'help-circle'
+          }
+        ]
+      ]
+    }
+  },
+  created () {
+    this.$store.commit('user', {
+      name: 'John Doe',
+      email: 'john@example.com',
+      avatar: '/data-sources/avatars/annie-spratt-121576-unsplash.jpg'
+    })
+  }
+}
+</script>

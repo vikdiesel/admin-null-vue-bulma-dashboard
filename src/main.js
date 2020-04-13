3845adb8cecdf0ce4c7ccdc765a46f7f87f74363
Viktor Kuzhelny <viktor@marketto.ru>
2020-04-13 19:31:50 +0500

    Add document titles

--- src/main.js
+++ src/main.js
@@ -19,9 +19,18 @@ import App from './App.vue'
 /* Vue. Component in recursion */
 import AsideMenuList from '@/components/AsideMenuList'
 
+/* Default title tag */
+const defaultDocumentTitle = 'Admin Null Bulma'
+
 /* Collapse mobile aside menu on route change */
-router.afterEach(() => {
+router.afterEach(to => {
   store.commit('asideMobileStateToggle', false)
+
+  if (to.meta.title) {
+    document.title = `${to.meta.title} — ${defaultDocumentTitle}`
+  } else {
+    document.title = defaultDocumentTitle
+  }
 })
 
 Vue.config.productionTip = false
--- src/router.js
+++ src/router.js
@@ -8,11 +8,19 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      // Document title tag
+      // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
+      meta: {
+        title: 'Dashboard'
+      },
       path: '/',
       name: 'home',
       component: Home
     },
     {
+      meta: {
+        title: 'Tables'
+      },
       path: '/tables',
       name: 'tables',
       // route level code-splitting
@@ -21,21 +29,33 @@ export default new Router({
       component: () => import(/* webpackChunkName: "tables" */ './views/Tables.vue')
     },
     {
+      meta: {
+        title: 'Forms'
+      },
       path: '/forms',
       name: 'forms',
       component: () => import(/* webpackChunkName: "forms" */ './views/Forms.vue')
     },
     {
+      meta: {
+        title: 'Profile'
+      },
       path: '/profile',
       name: 'profile',
       component: () => import(/* webpackChunkName: "profile" */ './views/Profile.vue')
     },
     {
+      meta: {
+        title: 'New client'
+      },
       path: '/client/new',
       name: 'client.new',
       component: () => import(/* webpackChunkName: "client-form" */ './views/ClientForm.vue')
     },
     {
+      meta: {
+        title: 'Edit client'
+      },
       path: '/client/:id',
       name: 'client.edit',
       component: () => import(/* webpackChunkName: "client-form" */ './views/ClientForm.vue'),
